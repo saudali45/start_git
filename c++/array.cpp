96a14faf69e9921a87dc1f35afe26eca56f0cb46
saud Ali <saudali4526@gmail.com>
2026-03-22 20:08:56 +0530

    v1

---
+++ c++/array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std ;
+int swap_2(int &array,int n){
+  
+    for (int  i = 0; i <n ; i++)
+    {
+
+        for (int j = 0; j < n; i++){
+             
+        }
+    //  dgd/
+        
+    }
+    
+}
+    int main(){
+    
+    return 0;
+}
